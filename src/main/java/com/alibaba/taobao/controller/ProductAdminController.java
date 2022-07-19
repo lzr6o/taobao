a7355c8ddc8136a501bfb38c0762bdbf908f2881
李zr <zr6oli@gmail.com>
2022-07-19 01:22:48 -0400

    edit

--- src/main/java/com/alibaba/taobao/controller/ProductAdminController.java
+++ src/main/java/com/alibaba/taobao/controller/ProductAdminController.java
@@ -1,23 +1,30 @@
 package com.alibaba.taobao.controller;
 
 import com.alibaba.taobao.common.ApiRestResponse;
+import com.alibaba.taobao.common.Constant;
+import com.alibaba.taobao.exception.AlibabaTaobaoException;
+import com.alibaba.taobao.exception.AlibabaTaobaoExceptionEnum;
 import com.alibaba.taobao.model.request.AddProductReq;
 import com.alibaba.taobao.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.UUID;
 
 /**
  * 描述: 后台商品管理Controller
  */
-@Controller
+@RestController
 public class ProductAdminController {
 
     @Autowired
@@ -37,6 +44,32 @@ public class ProductAdminController {
         UUID uuid = UUID.randomUUID();
         String newFileName = uuid.toString() + suffixName;
         // 创建文件
-        
+        File fileDirectory = new File(Constant.FILE_UPLOAD_DIR);
+        File destFile = new File(Constant.FILE_UPLOAD_DIR + newFileName);
+        if (!fileDirectory.exists()) {
+            if (!fileDirectory.mkdir()) {
+                throw new AlibabaTaobaoException(AlibabaTaobaoExceptionEnum.MKDIR_FAILED);
+            }
+        }
+        try {
+            file.transferTo(destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return ApiRestResponse.success(getHost(new URI(httpServletRequest.getRequestURI() + "")) + "/images/" + newFileName);
+        } catch (URISyntaxException e) {
+            return ApiRestResponse.error(AlibabaTaobaoExceptionEnum.UPLOAD_FAILED);
+        }
+    }
+
+    private URI getHost(URI uri) {
+        URI effectiveURI;
+        try {
+            effectiveURI = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), null, null, null);
+        } catch (URISyntaxException e) {
+            effectiveURI = null;
+        }
+        return effectiveURI;
     }
 }
